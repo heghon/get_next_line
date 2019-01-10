86685e2544e0f3043a8dcdcb92614e2d378d3452
heghon <46563964+heghon@users.noreply.github.com>
2019-01-10 14:15:36 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmenant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/29 16:21:20 by bmenant           #+#    #+#             */
+/*   Updated: 2019/01/09 12:16:51 by bmenant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
